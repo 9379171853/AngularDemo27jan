4ea8f8c6c8cb54e042dab7d6b3ab976590d71597
amrendra kumar <amarmca51@gmail.com>
2019-01-27 17:11:26 +0530

    project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  //title = 'app';
+  showSecret = false;
+  log = [];
+
+  onToggleDetails(){
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+    
+  }
+
 }
